474ca97866cd354a33144c3a10126a8161dbfda5
rendykml <rendikamaluddin2018@gmail.com>
2026-01-08 23:32:28 +0700

    page dari log history stock

--- src/pages/stock/LogsPage.jsx
+++ src/pages/stock/LogsPage.jsx
@@ -35,7 +35,7 @@ export default function LogsPage() {
 
       <ComponentCard className="p-0">
         {/* HEADER */}
-        <div className="border-b px-5 py-4">
+        <div className="border-b px-5 py-4 border-gray-100 dark:border-gray-500">
           <h2 className="text-base font-medium text-gray-800 dark:text-white/90">
             Riwayat Stok Gudang
           </h2>
