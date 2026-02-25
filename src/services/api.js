c82d91c74357a6e253f5883172b93ce51354ca16
rendykml <rendikamaluddin2018@gmail.com>
2026-02-26 06:35:19 +0700

    rubah base url lagi

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
-  // baseURL: "https://possystembe-production-b296.up.railway.app/api",
+  // baseURL: "http://localhost:8000/api",
+  baseURL: "https://possystembe-production-b296.up.railway.app/api",
   headers: {
     Accept: "application/json",
   },
